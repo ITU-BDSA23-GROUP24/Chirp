153ad22daba43e08560e0e5b2b04c8a152f530cc
Anton <avad@itu.dk>
2023-08-31 16:52:44 +0200

    Refactor main to read and print from .csv file using Regex
    Co-authered-by: Frederik <frbb@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.RegularExpressions;
+
+if(args.Length == 0)
+    return;
+if(args[0] == "read")
+{
+    var sr = new StreamReader("data/chirp_cli_db.csv");
+    
+    sr.ReadLine();
+    while(sr.Peek() >= 0)
+    {
+        var data = Regex.Split(sr.ReadLine(),@",""|"",");
+        Console.WriteLine($"{data[0]} @ {data[2]}: {data[1]}");
+    }
+}
