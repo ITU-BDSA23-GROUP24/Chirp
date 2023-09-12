b3ec30416b39ea23c2d35418863f5071f46ffc0f
David <drin@itu.dk>
2023-09-12 12:53:26 +0200

    added comments to the userinterface wreitechirp since there were none
    added a summary and a paramiter discription to the writechirp function

--- Chirp.CLI/UserInterface.cs
+++ Chirp.CLI/UserInterface.cs
@@ -2,6 +2,10 @@ namespace Chirp.CLI;
 
 public class UserInterface
 {
+    /// <summary>
+    /// Takes a cheep and writes its to string in the console 
+    /// </summary>
+    /// <param name="ch">The given cheep that will be written in the console</param>
     public static void Writechirp(Cheep ch)
     {
         Console.WriteLine(ch.ToString());
