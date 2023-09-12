492daeb48356bf02f2b81df807a971127afd6461
Frederik B <frbb@itu.dk>
2023-09-12 15:01:11 +0200

    fixed issues created from merging
    there were a few issues that had to be fixed before the program worked

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,19 +1,12 @@
-// See https://aka.ms/new-console-template for more information
-using System.Data.Common;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 using Chirp.CLI;
 using DocoptNet;
-// See https://aka.ms/new-console-template for more information
-using System.Data.Common;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
-using DocoptNet;
 
-private static readonly string PathToCsvFile = "data/chirp_cli_db.csv";
+class Program
+{
+    private static readonly string PathToCsvFile = "data/chirp_cli_db.csv";
 
-//chirp --version in usage useless? still works when removed
-const string usage = @"Chirp.
+    //chirp --version in usage useless? still works when removed
+    const string usage = @"Chirp.
 
 Usage:
     chirp read
@@ -26,28 +19,38 @@ Options:
     --version   Show version.
 ";
 
-var arguments = new Docopt().Apply(usage, args, version: "Chirp 1.0", exit: true)!;
-if (arguments["read"].IsTrue){
-    ReadCheeps();
-}
-if (arguments["cheep"].IsTrue){
-    WriteCheep(arguments["<message>"].ToString());
-}
-/// <summary>
+    public static void Main(string[] args)
+    {
+        var arguments = new Docopt().Apply(usage, args, version: "Chirp 1.0", exit: true)!;
+        if (arguments["read"].IsTrue)
+        {
+            ReadCheeps();
+        }
+        else if (arguments["cheep"].IsTrue)
+        {
+            WriteCheep(arguments["<message>"].ToString());
+        }
+    }
+
+    /// <summary>
     /// Writes all cheep messages in the csv file to the console
     /// </summary>
-static void ReadCheeps(){
-    var cheeps = ChirpDataBase.Read(PathToCsvFile);
-    UserInterface.WriteChirp(cheeps);
-}
-/// <summary>
+    static void ReadCheeps()
+    {
+        var cheeps = ChirpDataBase.Read(PathToCsvFile);
+        UserInterface.PrintCheeps(cheeps);
+    }
+
+    /// <summary>
     /// Takes a string message, constructs a Cheep object, and writes it to the csv file
     /// </summary>
-    /// <param name="argText">The message sent as a cheep, to be written to the csv file</param>
-static void WriteCheep(string argText){
-    string userName = Environment.UserName;
-    string text = argText;
-    DateTimeOffset timestamp = DateTime.Now;
-    Cheep cheep = new Cheep(timestamp.ToUnixTimeSeconds(), userName, text);
-    ChirpDataBase.Write(PathToCsvFile, cheep);
+    /// <param name="message">The message sent as a cheep, to be written to the csv file</param>
+    static void WriteCheep(string message)
+    {
+        string author = Environment.UserName;
+        double timestamp = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
+        Cheep cheep = new Cheep(timestamp, author, message);
+        
+        ChirpDataBase.Write(PathToCsvFile, cheep);
+    }
 }
