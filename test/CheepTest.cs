5e3bb23a2e462e052ea00c5ece17d82fc5ea74e9
Frederik B <frbb@itu.dk>
2023-09-19 15:28:00 +0200

    replace var with explicit types
    as we will not use var in our project

--- test/CheepTest.cs
+++ test/CheepTest.cs
@@ -8,15 +8,15 @@ public class CheepTest
     [Fact]
     public void CheepConstruction()
     {
-        //arrange
+        // arrange
         double timestamp = 1695034276;
-        var author = "Henrik";
-        var message = "boomba";
+        string author = "Henrik";
+        string message = "boomba";
 
-        //act
-        var cheepTest = new Cheep(timestamp, author, message);
+        // act
+        Cheep cheepTest = new Cheep(timestamp, author, message);
 
-        //assert
+        // assert
         Assert.Equal(1695034276, cheepTest.Timestamp);
         Assert.Equal("Henrik", cheepTest.Author);
         Assert.Equal("boomba", cheepTest.Message);
@@ -29,10 +29,10 @@ public class CheepTest
     [Fact]
     public void CheepConstruction_EmptyAuthor_Exception()
     {
-        //arrange & act & assert
+        // arrange & act & assert
         Assert.Throws<ArgumentException>(() => new Cheep(1695034276, "", "Hello"));
     }
-    
+
     /// <summary>
     /// test if the Message argument in the Cheep constructor can be an empty string
     /// it should throw an ArgumentException error 
@@ -51,11 +51,11 @@ public class CheepTest
     [Fact]
     public void CheepConstruction_NegativeTimestamp_Exception()
     {
-        //arrange & act & assert
+        // arrange & act & assert
         Assert.Throws<ArgumentException>(() => new Cheep(-1695034267, "Hansen", "Nice"));
     }
 
-    
+
     /// <summary>
     /// test if the String arguments in the Cheep constructor can be null 
     /// it should throw an ArgumentNullException error 
@@ -66,26 +66,26 @@ public class CheepTest
     [InlineData(123456789, null, null)]
     public void CheepConstruction_NullArgument_Exception(double timeStamp, string author, string message)
     {
-        //arrange & act & assert
+        // arrange & act & assert
         Assert.Throws<ArgumentNullException>(() => new Cheep(timeStamp, author, message));
     }
 
-    
+
     /// <summary>
     /// test if the Cheep ToString() function formats the data correctly
     /// </summary>
     [Fact]
     public void cheep_ToString()
     {
-        //arrange
+        // arrange
         double timestamp = 1695034276;
-        var author = "Henrik";
-        var message = "boomba";
+        string author = "Henrik";
+        string message = "boomba";
 
-        //act
-        var cheepTest = new Cheep(timestamp, author, message);
+        // act
+        Cheep cheepTest = new Cheep(timestamp, author, message);
 
-        //assert
+        // assert
         Assert.Equal("Henrik @ 18-09-2023 12:51:16: boomba", cheepTest.ToString());
     }
 }
