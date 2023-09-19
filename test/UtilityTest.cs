270861fcb721caaa7b674659b87a913ae513608b
Frederik B <frbb@itu.dk>
2023-09-19 17:18:01 +0200

    added summaries to utility tests
    added summaries to the unixTime utility tests as they were missing

--- test/UtilityTest.cs
+++ test/UtilityTest.cs
@@ -2,8 +2,11 @@ namespace test;
 
 public class UtilityTest
 {
+    /// <summary>
+    /// Tests if the unixTile to DateTime conversion function throws an ArgumentException,
+    /// when the UnixTimeStamp is negative
+    /// </summary>
     [Fact]
-
     public void Utility_UnixTimeStampToDateTime_NegativeUnixTime_ArgumentException()
     {
         // arrange
@@ -12,19 +15,22 @@ public class UtilityTest
         Assert.Throws<ArgumentException>(() => Utility.UnixTimeStampToDateTime(timeStamp));
     }
 
+    /// <summary>
+    /// checks that the conversion of the function is correct.
+    /// </summary>
     [Theory]
     [InlineData(1695126565, 2023, 9, 19, 14, 29, 25)]
-    [InlineData(977691600,2000,12,24,22,0,0)]
-    [InlineData(3133650000,2069,4,20,4,20,0)]
-    public void Utility_UnixTimeStampToDateTime(double timeStamp,int year, int month,int day,int hour,int minute,int second)
+    [InlineData(977691600, 2000, 12, 24, 22, 0, 0)]
+    [InlineData(3133650000, 2069, 4, 20, 4, 20, 0)]
+    public void Utility_UnixTimeStampToDateTime(double timeStamp, int year, int month, int day, int hour, int minute,
+        int second)
     {
         // arrange
         DateTime datetime = new DateTime(year, month, day, hour, minute, second);
         // act
-        DateTime dateTime_fromUnixTimeStamp = Utility.UnixTimeStampToDateTime(timeStamp);
-        
+        DateTime dateTimeFromUnixTimeStamp = Utility.UnixTimeStampToDateTime(timeStamp);
+
         // assert
-        Assert.Equal(datetime,dateTime_fromUnixTimeStamp);   
+        Assert.Equal(datetime, dateTimeFromUnixTimeStamp);
     }
-    
 }
