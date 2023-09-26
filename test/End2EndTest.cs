cbdffe94db2fad0672629da5f4bfa6b6bb419c33
Frederik B <frbb@itu.dk>
2023-09-26 11:41:27 +0200

    cant fix error 'permission denied'

--- test/End2EndTest.cs
+++ test/End2EndTest.cs
@@ -1,18 +1,42 @@
+using System.Diagnostics;
+using SimpleDB;
+
 namespace test;
 
 public class End2EndTest
 {
+    private const string PathToTestCsvFile = "./testdata/chirp_cli_test_db.csv";
+    private IDatabase<Cheep> testDatabase = CSVDatabase<Cheep>.Instance;
+    
+    /// <summary>
+    /// Sets up the csv database with 12 cheeps of test data.
+    /// </summary>
+    private void SetupTestCsvDatabase()
+    {
+        testDatabase = CSVDatabase<Cheep>.Instance;
+
+        // delete test file if it already exists
+        if (File.Exists(PathToTestCsvFile)) File.Delete(PathToTestCsvFile);
+        testDatabase.SetFilePath(PathToTestCsvFile);
+        
+        for (int i = 0; i < 12; i++)
+        {
+            testDatabase.Store(new Cheep(1690891760, "testAuthor" + i, "testMessage" + i));
+        }
+    }
+    
     [Fact]
     public void TestReadCheep()
     {
         // Arrange
-        ArrangeTestDatabase();
+        SetupTestCsvDatabase();
+        
         // Act
-        string output = "";
+        string output;
         using (var process = new Process())
         {
-            process.StartInfo.FileName = "/usr/bin/dotnet";
-            process.StartInfo.Arguments = "./src/Chirp.CLI.Client/bin/Debug/net7.0/chirp.dll read 10";
+            process.StartInfo.FileName = "Chirp.dll";
+            process.StartInfo.Arguments = "./src/Chirp.CLI/bin/Debug/net7.0/Chirp.dll read 10";
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.WorkingDirectory = "../../../../../";
             process.StartInfo.RedirectStandardOutput = true;
@@ -23,8 +47,9 @@ public class End2EndTest
             process.WaitForExit();
         }
         string fstCheep = output.Split("\n")[0];
+        
         // Assert
-        Assert.StartsWith("ropf", fstCheep);
-        Assert.EndsWith("Hello, World!", fstCheep);
+        Assert.StartsWith("testAuthor0", fstCheep);
+        Assert.EndsWith("testMessage0", fstCheep);
     }
 }
