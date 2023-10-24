5816df855dbe85e8397de9645661c30ed3245776
Frederik B <frbb@itu.dk>
2023-10-24 13:34:11 +0200

    fix issues
    Co-authored-by: David <drin@itu.dk>

--- src/Chirp.Infrastructure/ChirpDBContext.cs
+++ src/Chirp.Infrastructure/ChirpDBContext.cs
@@ -5,21 +5,8 @@ namespace Chirp.Infrastructure;
 
 public class ChirpDBContext : DbContext
 {
-    public DbSet<Author> Authors { get; set; }
-    public DbSet<Cheep> Cheeps { get; set; }
+    public DbSet<Author> Authors => Set<Author>();
+    public DbSet<Cheep> Cheeps => Set<Cheep>();
 
-    public string DbPath { get; }
-
-    public ChirpDBContext()
-    {
-        string dbFileName = "Chirp.db";
-        string folderPath = Path.GetFullPath(".");
-        DbPath = Path.Combine(folderPath, dbFileName);
-        // Console.WriteLine($"Saved SQLite DB at: {DbPath}");
-    }
-
-    // The following configures EF to create a Sqlite database file in the
-    // special "local" folder for your platform.
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
+    public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options) { }
 }
