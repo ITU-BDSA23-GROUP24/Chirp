db9c773d3bfd45f8e08f731f5a7b13d2f37377e7
Emil Fenger <fenger.emil@gmail.com>
2023-10-31 12:48:45 +0100

    Cheep repository init

--- test/CheepRepositoryTest.cs
+++ test/CheepRepositoryTest.cs
@@ -1,1 +1,20 @@
+using Chirp.Infrastructure;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
+
+
+public class CheepRepositoryTest {
+
+    public CheepRepository setupDatabase() {
+        using var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+        using var context = new ChirpDBContext(builder.Options);
+        return new CheepRepository(context);
+    }
+
+    public void testGetPageOfCheeps() {
+        
+    }
+}
