08130bbb203e4b7fceb7d03ab43c62b1f38300ce
David <drin@itu.dk>
2023-10-31 14:48:21 +0100

    added  more and updated testes for pagenation
    Co-authored-by: Frederik <frbb@itu.dk>

--- test/IntegrationTesting.cs
+++ test/IntegrationTesting.cs
@@ -20,6 +20,11 @@ public class IntegrationTesting : IClassFixture<WebApplicationFactory<Program>>
             { AllowAutoRedirect = true, HandleCookies = true });
     }
 
+    /// <summary>
+    /// this test checks that we are on a chirp site and if we can access the public timeline
+    /// by asserting that the html code on our frontpage/publictimeline contains the app title text Chirp!
+    /// and the page title public timeline.
+    /// </summary>
     [Fact]
     public async void CanSeePublicTimeline()
     {
@@ -32,7 +37,10 @@ public class IntegrationTesting : IClassFixture<WebApplicationFactory<Program>>
         Assert.Contains("Chirp!", content);
         Assert.Contains("Public Timeline", content);
     }
-
+/// <summary>
+///  this test checks that we go to a users private timeline when we specify a user in the url 
+/// </summary>
+/// <param name="author"> the specified user</param>
     [Theory]
     [InlineData("Helge")]
     [InlineData("Rasmus")]
@@ -48,6 +56,11 @@ public class IntegrationTesting : IClassFixture<WebApplicationFactory<Program>>
         Assert.Contains($"{author}'s Timeline", content);
     }
 
+/// <summary>
+/// here we cheek that there are exactly 32 cheeps in a full page of cheeps
+/// both on the public and on a private timeline
+/// </summary>
+/// <param name="page">the specified page(public or private)</param>
     [Theory]
     [InlineData("")]
     [InlineData("Jacqualine Gilcoine")]
@@ -64,28 +77,43 @@ public class IntegrationTesting : IClassFixture<WebApplicationFactory<Program>>
         string cheepListStr = cheepListMatches[0].ToString();
 
         int listElementCount = Regex.Matches(cheepListStr, "<li>").Count;
+        
+        response = await client.GetAsync($"/{page}?page=2");
+        response.EnsureSuccessStatusCode();
+        string page2 = await response.Content.ReadAsStringAsync();
+        
+        string contentOneLinePage2 = Regex.Replace(content, "\n", "");
+        GroupCollection cheepListMatchesPage2 =
+            Regex.Match(contentOneLine, "<ul id=\"messagelist\" class=\"cheeps\">.*<\\/ul>").Groups;
+        string cheepListStrPage2 = cheepListMatches[0].ToString();
+        int listElementCountPage2 = Regex.Matches(cheepListStr, "<li>").Count;
+
 
         // Assert
         Assert.Contains("Chirp!", content);
         Assert.Single(cheepListMatches);
         Assert.Equal(32, listElementCount);
+        Assert.True(listElementCountPage2 <= 32);
     }
-
+/// <summary>
+/// 
+/// </summary>
+/// <param name="author"></param>
     [Theory]
     [InlineData("")]
     [InlineData("Jacqualine Gilcoine")]
-    public async void PaginationTest(string author)
+    public async void PaginationTest_MoreThanOnePage(string author)
     {
         // Act
         // get HTML
         HttpResponseMessage response = await client.GetAsync($"/{author}");
         response.EnsureSuccessStatusCode();
         string page = await response.Content.ReadAsStringAsync();
-        
+
         response = await client.GetAsync($"/{author}?page=1");
         response.EnsureSuccessStatusCode();
         string page1 = await response.Content.ReadAsStringAsync();
-        
+
         response = await client.GetAsync($"/{author}?page=2");
         response.EnsureSuccessStatusCode();
         string page2 = await response.Content.ReadAsStringAsync();
@@ -108,8 +136,25 @@ public class IntegrationTesting : IClassFixture<WebApplicationFactory<Program>>
         Assert.Contains("Chirp!", page);
         Assert.Contains("Chirp!", page1);
         Assert.Contains("Chirp!", page2);
-        
+
         Assert.Equal(firstCheeps[0], firstCheeps[1]);
         Assert.NotEqual(firstCheeps[1], firstCheeps[2]);
+    }
+
+
+    [Theory]
+    [InlineData("Jacqualine Gilcoine?page=999")]
+    [InlineData("Non Existing Author")]
+    public async void PaginationTest_PageNotFound(string author)
+    {
+        // Act
+        // get HTML
+        HttpResponseMessage response = await client.GetAsync($"/{author}");
+        response.EnsureSuccessStatusCode();
+        string page = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Contains("Chirp!", page);
+        Assert.Contains("There are no cheeps here.", page);
     }
 }
