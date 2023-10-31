a888d18dce3d77868a12e8dbf50bea91291d5a04
Oskar <oskw@itu.dk>
2023-10-31 12:52:31 +0100

    AuthorRepo setup database

--- test/AuthorRepositoryTest.cs
+++ test/AuthorRepositoryTest.cs
@@ -1,1 +1,19 @@
 using Xunit;
+using Chirp.Infrastructure;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+
+public class AuthorRepositoryTest {
+
+    public AuthorRepository setupDatabase() {
+        using var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+        using var context = new ChirpDBContext(builder.Options);
+        return new AuthorRepository(context);
+    }
+
+    
+}
