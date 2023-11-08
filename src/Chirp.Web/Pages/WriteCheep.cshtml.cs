430e56f6ff8ff7d1c7c7af4979deca4eae8d4f16
Frederik B <frbb@itu.dk>
2023-11-08 11:58:27 +0100

    Added write cheep page (without login)

---
+++ src/Chirp.Web/Pages/WriteCheep.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages
+{
+    public class PostCheepModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
