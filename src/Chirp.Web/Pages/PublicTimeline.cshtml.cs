0d56c57279fc4c8e033dec243e02d0e1b95e73e4
Oskar <oskw@itu.dk>
2023-11-15 20:04:24 +0100

    all pagination working
    Next, previous, and "nearby" page number links work, and are updated correctly depending on the current page, both for public and user timelines

--- src/Chirp.Web/Pages/PublicTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/PublicTimeline.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Chirp.Core;
+﻿using System.Runtime.CompilerServices;
+using Chirp.Core;
 using Chirp.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,8 +13,13 @@ public class PublicModel : PageModel
 
     [BindProperty(SupportsGet = true)]
     public int currentPage {get; set;}
-    int totalPages {get; set;}
+    public int totalPages {get; set;}
     int pageSize {get; set;}
+
+    public int navigationNumber {get; set;}
+
+    public List<int> numbersToShow {get; set;}
+
 
     int count {get; set;}
 
@@ -24,6 +30,11 @@ public class PublicModel : PageModel
         this.cheepRepository = cheepRepository;
         Cheeps = new List<CheepViewModel>();
         pageSize = 32;
+        totalPages = 1;
+        //The amount of pages that are shown between the "previous" and "next" button
+        //Should always be odd, such that the current page can be in the center when relevant
+        navigationNumber = 7;
+        numbersToShow = new List<int>();
     }
 
     public async Task<ActionResult> OnGetAsync([FromQuery] int page)
@@ -31,6 +42,7 @@ public class PublicModel : PageModel
         if (page < 1) page = 1;
 
         currentPage = page;
+        numbersToShow.Clear();
 
         try
         {
@@ -38,12 +50,30 @@ public class PublicModel : PageModel
             Cheeps = cheeps.ToList();
             count = await cheepRepository.GetCheepCountAll();
             totalPages = count/pageSize;
+            //Adds one extra page if the amount if cheeps is not perfectly divisible by the page size, where the remaining cheeps can be shown
             if (count%pageSize != 0){
                 totalPages++;
+            }
+            int middleNumber = navigationNumber/2+1;
+            if (currentPage-navigationNumber/2 < 1){
+                for (int i = 1 ; i <= navigationNumber ; i++){
+                    numbersToShow.Add(i);
+                }
+            }
+            else if (currentPage + navigationNumber/2 > totalPages){
+                for (int i = totalPages-navigationNumber; i <= totalPages ; i++){
+                    numbersToShow.Add(i);
+                }
+            }
+            else {
+                for (int i = currentPage - navigationNumber/2; i <= currentPage + navigationNumber/2; i++){
+                    numbersToShow.Add(i);
+                }
             }
         }
         catch
         {
+            //empty list of cheeps if there are no cheeps to show, this is caught by the cshtml and shown as "There are no cheeps here"
             Cheeps = new List<CheepViewModel>();
         }
 
--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -10,23 +10,75 @@ public class UserTimelineModel : PageModel
     private readonly ICheepRepository cheepRepository;
     public List<CheepViewModel> Cheeps { get; set; }
 
+    [BindProperty(SupportsGet = true)]
+    public int currentPage {get; set;}
+    public int totalPages {get; set;}
+    int pageSize {get; set;}
+
+    public int navigationNumber {get; set;}
+
+    public List<int> numbersToShow {get; set;}
+
+    public string navigationAuthor {get; set;}
+
+
+    int count {get; set;}
+
     public UserTimelineModel(ICheepRepository cheepRepository)
     {
         this.cheepRepository = cheepRepository;
         Cheeps = new List<CheepViewModel>();
+        pageSize = 32;
+        totalPages = 1;
+        //The amount of pages that are shown between the "previous" and "next" button
+        //Should always be odd, such that the current page can be in the center when relevant
+        navigationNumber = 7;
+        numbersToShow = new List<int>();
+        //
+        navigationAuthor = "";
     }
 
     public async Task<ActionResult> OnGetAsync(string author, [FromQuery] int page)
     {
         if (page < 1) page = 1;
 
+        currentPage = page;
+        numbersToShow.Clear();
+
+        //slightly wonky code necessary for the cshtml file to find the current author through Model.navigationAuthor
+        //as it cannot directly access the author argument passed to OnGetAsync
+        navigationAuthor = author;
+
         try
         {
             IEnumerable<CheepViewModel> cheeps = await cheepRepository.GetPageOfCheepsByAuthor(author, page);
             Cheeps = cheeps.ToList();
+            count = await cheepRepository.GetCheepCountAuthor(author);
+            totalPages = count/pageSize;
+            //Adds one extra page if the amount if cheeps is not perfectly divisible by the page size, where the remaining cheeps can be shown
+            if (count%pageSize != 0){
+                totalPages++;
+            }
+            int middleNumber = navigationNumber/2+1;
+            if (currentPage-navigationNumber/2 < 1){
+                for (int i = 1 ; i <= navigationNumber ; i++){
+                    numbersToShow.Add(i);
+                }
+            }
+            else if (currentPage + navigationNumber/2 > totalPages){
+                for (int i = totalPages-navigationNumber; i <= totalPages ; i++){
+                    numbersToShow.Add(i);
+                }
+            }
+            else {
+                for (int i = currentPage - navigationNumber/2; i <= currentPage + navigationNumber/2; i++){
+                    numbersToShow.Add(i);
+                }
+            }
         }
         catch
         {
+            //empty list of cheeps if there are no cheeps to show, this is caught by the cshtml and shown as "There are no cheeps here"
             Cheeps = new List<CheepViewModel>();
         }
 
