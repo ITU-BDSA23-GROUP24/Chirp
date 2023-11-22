8813bf9b1014c8271ae44baafdcf69e6d26bec01
Emil Fenger <fenger.emil@gmail.com>
2023-11-22 13:43:29 +0100

    improved environment variables

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -10,16 +10,16 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Configuration.AddUserSecrets("id");
-builder.Configuration.AddEnvironmentVariables();
-var AZURE_DB_SERVER = builder.Configuration["AZURE_DB_SERVER"];
-var AZURE_DB_NAME = builder.Configuration["AZURE_DB_NAME"];
-var AZURE_DB_USER = builder.Configuration["AZURE_DB_USER"];
-var AZURE_DB_PASSWORD = builder.Configuration["AZURE_DB_PASSWORD"];
 if (builder.Configuration.GetConnectionString("Chirp") != null) {
     builder.Services.AddDbContext<ChirpDBContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("Chirp")));
 }
 else {
+    builder.Configuration.AddEnvironmentVariables();
+    var AZURE_DB_SERVER = builder.Configuration["AZURE_DB_SERVER"];
+    var AZURE_DB_NAME = builder.Configuration["AZURE_DB_NAME"];
+    var AZURE_DB_USER = builder.Configuration["AZURE_DB_USER"];
+    var AZURE_DB_PASSWORD = builder.Configuration["AZURE_DB_PASSWORD"];
     var connectionString = $"Server={AZURE_DB_SERVER};Initial Catalog={AZURE_DB_NAME};Persist Security Info=False;User ID={AZURE_DB_USER};Password={AZURE_DB_PASSWORD};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     builder.Services.AddDbContext<ChirpDBContext>(options =>
         options.UseSqlServer(connectionString));
