7272b178d9e861872c6b8fff92ebed2d30153472
Anton <avad@itu.dk>
2023-12-20 11:08:56 +0100

    merge 2.0

--- src/Chirp.Core/IAuthorRepository.cs
+++ src/Chirp.Core/IAuthorRepository.cs
@@ -1,10 +1,8 @@
 namespace Chirp.Core;
 public interface IAuthorRepository
 {
-    Task CreateAuthor(string authorName, string authorEmail);
+    Task CreateAuthor(string authorName);
     Task RemoveAuthor(string authorName);
     Task<AuthorViewModel> FindAuthorByName(string authorName);
-    Task<AuthorViewModel> FindAuthorByEmail(string authorEmail);
     Task<bool> DoesUserNameExists(string authorName);
-    Task<bool> DoesUserEmailExists(string authorEmail);
 }
